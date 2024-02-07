886e359ee50923149fdae3d8282a716a5b21c572
Bodlj001 <jackso.bodleyscott@gmail.com>
2024-02-07 16:52:39 +0000

    frontend added

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,10 +14,14 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'lime-25': '#F7FCFC',
-        'ofset-30': '#2BCED2',
-        'ofset-60': '#26474E',
-        'text-10': '#26474E'
+        'lime-25': '#F5F5F5',
+        'ofset-30': '#37A6A6',
+        'ofset-60': '#016263',
+        'text-10': '#2BCED2',
+        'darkgray-90': '#949494',
+        'yorange-30': '#FFAA01',
+        'yorange-60': '#FEBF40',
+        'ourred': '#FF0000'
       },
     },
   },
